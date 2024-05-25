177623c771e8cd3b80743302f987b72a67dc122b
CarlosYcochea <134462386+CarlosYcochea@users.noreply.github.com>
2024-05-25 03:18:34 -0400

    dashboard primera parte terminada

--- dashboard.js
+++ dashboard.js
@@ -6,3 +6,22 @@ function activarLink(){
 }
 lista.forEach((item) =>
     item.addEventListener('mouseover', activarLink));
+
+var toggle = document.querySelector('.toggle');
+var nav = document.querySelector('.nav');
+var container = document.querySelector('.container');
+
+toggle.onclick = function() {
+    nav.classList.toggle('active');
+    container.classList.toggle('active');
+}
+
+function mostrarTabla() {
+    var table = document.getElementById("miTabla");
+    table.style.display = "table";
+}
+
+function ocultarTabla() {
+    var table = document.getElementById("miTabla");
+    table.style.display = "none";
+}
