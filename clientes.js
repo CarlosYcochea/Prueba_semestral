ab9b411b34903555fa07a0629dd4d90641b856a7
CarlosYcochea <134462386+CarlosYcochea@users.noreply.github.com>
2024-05-25 07:18:15 -0400

    clientes 2

--- clientes.js
+++ clientes.js
@@ -15,3 +15,37 @@ toggle.onclick = function() {
     nav.classList.toggle('active');
     container.classList.toggle('active');
 }
+
+$(document).ready(function() {
+    $('#formulario-cliente').on('submit', function(event) {
+        event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
+
+        // Obtén los valores de los campos del formulario
+        var nombre = $('#Nombre').val();
+        var rut = $('#Rut').val();
+        var email = $('#Email').val();
+        var telefono = $('#telefono').val();
+        var direccion = $('#direccion').val();
+
+        // Agrega una nueva fila a la tabla con los datos proporcionados por el usuario
+        var nuevaFila = `
+            <tr>
+                <td>${nombre}</td>
+                <td>${rut}</td>
+                <td>${email}</td>
+                <td>${telefono}</td>
+                <td>${direccion}</td>
+            </tr>
+        `;
+        $('.tabla-clientes tbody').append(nuevaFila);
+
+        // Limpia los campos del formulario
+        $('#formulario-cliente')[0].reset();
+    });
+});
+
+
+
+
+
+    
